bd2a35ac0157ae326b34685d9ece5d6978931951
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-08-19 16:01:42 +0545

    signin protectedroute

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,37 +1,36 @@
 import { useEffect, ReactNode } from "react";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { fetchProfile } from "./store/authSlice";
-// import { useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
-
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const dispatch = useAppDispatch();
-  // const navigate = useNavigate()
+  const navigate = useNavigate();
   const { data } = useAppSelector((state) => state.auth);
 
-useEffect(()=>{
-      dispatch(fetchProfile())
-     
-},[dispatch])
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      dispatch(fetchProfile());
+    } else {
+      navigate("/signin");
+    }
+  }, [dispatch, navigate]);
 
-// if(!token){
-//   navigate("/signin")
-//   return null;
-// }
 
-  
-  if(data?.role == "admin"){
+  // Render children if user is admin
+  if (data?.role === "admin") {
+    return <>{children}</>;
+  } else {
     return (
-      <>{children}</>
-    )
-  }else{
-    return(
-      <>You don't have permission</>
-    )
+      <>
+        <div className="text-white">You don't have permission</div>
+      </>
+    );
   }
 };
 
